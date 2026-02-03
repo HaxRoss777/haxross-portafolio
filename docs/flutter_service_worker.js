34fe06a676757d845dd6b3bf35c0dc9f4c2aaa98
Hayron <haxrosshdez@gmail.com>
2026-02-03 04:19:09 -0500

    Update ContactFormWidget: yellow focus border

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -55,7 +55,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a0a65b3cf0c2067a52dec567aa1bd693
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "31372898420392a4f348985154dd8f33",
+"flutter_bootstrap.js": "7c99605aefb85fc8b01a8b4aea24b6f0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
